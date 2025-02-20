01df241a2860cfb6d7f5f7f57a351c20a43334e1
Orekidesu <cmcodog10@gmail.com>
2025-02-20 17:59:10 +0800

    disabled csrf token and now only focused on bearer token as it should

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends HttpKernel
 
     protected $routeMiddleware = [
         'role' => \App\Http\Middleware\RoleMiddleware::class,
-    ];  
+    ];
     /**
      * The application's route middleware groups.
      *
@@ -42,8 +42,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
