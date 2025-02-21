f2a9e9efcc3283f47c53594731881c9187f39c00
Orekidesu <cmcodog10@gmail.com>
2025-02-22 02:44:34 +0800

    Added a name attribute in the GA table

--- database/migrations/2025_02_02_170650_create_departments_table.php
+++ database/migrations/2025_02_02_170650_create_departments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('abbreviation')->unique();
+            $table->foreignId('faculty_id')->constrained('faculties')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
--- database/migrations/2025_02_16_151940_create_graduate_attributes_table.php
+++ database/migrations/2025_02_16_151940_create_graduate_attributes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('graduate_attributes', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger("ga_no");
+            $table->string("name")->unique();
             $table->text('description');
             $table->softDeletes();
             $table->timestamps();
