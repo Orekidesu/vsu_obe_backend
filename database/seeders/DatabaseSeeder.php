40ad658d836fa40c8a4ee2c66b74cf691b59a00e
Orekidesu <cmcodog10@gmail.com>
2025-02-23 16:12:56 +0800

    - Added a seeder for GAs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(FacultySeeder::class);
-        $this->call(DepartmentSeeder::class);
+        // $this->call(FacultySeeder::class);
+        // $this->call(DepartmentSeeder::class);
+        $this->call(GraduateAttributeSeeder::class);
     }
 }
