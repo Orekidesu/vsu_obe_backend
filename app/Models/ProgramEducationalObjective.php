53532829aae09fb8d4e1b346d1b659e28b6594e6
Orekidesu <cmcodog10@gmail.com>
2025-03-12 20:29:21 +0800

    -add Program and ProgramEducationalObjective models with migrations and factories -added a relationship for department,program,and peo

---
+++ app/Models/ProgramEducationalObjective.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProgramEducationalObjective extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'program_id',
+        'peo_no',
+        'statement',
+    ];
+
+    public function program(): BelongsTo
+    {
+        return $this->belongsTo(Program::class, 'program_id');
+    }
+}
