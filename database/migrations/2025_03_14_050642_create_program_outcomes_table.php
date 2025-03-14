81d9a23998254bab912a93ac781488a35a38cd30
Orekidesu <cmcodog10@gmail.com>
2025-03-14 20:37:31 +0800

    feat: add foreign key constraint for program_id in program_outcomes table

--- database/migrations/2025_03_14_050642_create_program_outcomes_table.php
+++ database/migrations/2025_03_14_050642_create_program_outcomes_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->text('statement');
+            $table->foreignId('program_id')->constrained('programs')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
