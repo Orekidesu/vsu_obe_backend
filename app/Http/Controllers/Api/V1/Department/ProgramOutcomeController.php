e94da94d8ada5a67fe502a371860f8876d946da3
Orekidesu <cmcodog10@gmail.com>
2025-03-14 14:13:47 +0800

    feat: add ProgramOutcomeController with resource methods

---
+++ app/Http/Controllers/Api/V1/Department/ProgramOutcomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Department;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProgramOutcome;
+use Illuminate\Http\Request;
+
+class ProgramOutcomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProgramOutcome $programOutcome)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProgramOutcome $programOutcome)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProgramOutcome $programOutcome)
+    {
+        //
+    }
+}
