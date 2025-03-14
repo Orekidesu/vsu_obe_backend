ea7e1431afc61cedc451b515c17feefa937f1b48
Orekidesu <cmcodog10@gmail.com>
2025-03-14 14:17:20 +0800

    feat: add ProgramOutcomeResource for transforming program outcome data

---
+++ app/Http/Resources/Api/V1/Department/ProgramOutcomeResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Department;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProgramOutcomeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'program' => new ProgramResource($this->whenLoaded('program')),
+            'name' => $this->name,
+            'statement' => $this->statement,
+        ];
+    }
+}
