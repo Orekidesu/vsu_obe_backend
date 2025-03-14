4e6e5deebe01bbc80f7c89e20c80605caee05235
Orekidesu <cmcodog10@gmail.com>
2025-03-14 14:02:40 +0800

    refactor: remove peo_no from ProgramEducationalObjectiveResource response

--- app/Http/Resources/Api/V1/Department/ProgramEducationalObjectiveResource.php
+++ app/Http/Resources/Api/V1/Department/ProgramEducationalObjectiveResource.php
@@ -15,7 +15,6 @@ class ProgramEducationalObjectiveResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'peo_no' => $this->peo_no,
             'statement' => $this->statement,
             'program' => new ProgramResource($this->whenLoaded('program')),
         ];
