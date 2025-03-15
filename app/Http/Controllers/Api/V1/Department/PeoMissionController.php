ee93cd0a11dc984bdcb9a0d688c56c83c564f351
Orekidesu <cmcodog10@gmail.com>
2025-03-16 03:18:48 +0800

    feat: implement PeoMissionController for managing PEO-Missions mappings

---
+++ app/Http/Controllers/Api/V1/Department/PeoMissionController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Department;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\Department\PeoMissionRequest;
+use App\Models\ProgramEducationalObjective;
+use Illuminate\Http\Request;
+use Exception;
+
+class PeoMissionController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+        $this->middleware('role:Department');
+    }
+
+
+    // get all mapped peos with their corresponding missions
+    public function index()
+    {
+        try {
+
+            return response()->json([
+                'data' => ProgramEducationalObjective::with('missions')->get(),
+                'message' => 'All PEO-Missions mappings retrieved successfully',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'failed to retrieved PEO-Missions Mappings',
+                'error' => $e->getMessage(),
+
+            ], 500);
+        }
+    }
+
+    // get missions for a specific PEO
+    public function show(ProgramEducationalObjective $peo)
+    {
+        try {
+            $peo->load('missions');
+            return response()->json([
+                'data' => $peo,
+                'message' => 'PEO with mapped missions retrieved successfully'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'failed to fetch PEO with mapped missions',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    //attach multiple missions to a peo (Bulk Insert)
+    public function attach(PeoMissionRequest $request, ProgramEducationalObjective $peo)
+    {
+
+        try {
+            $peo->missions()->syncWithoutDetaching($request->validated());
+
+            return response()->json([
+                'message' => 'Missions mapped to PEO successfully'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'failed to map missions to PEO',
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function detach(PeoMissionRequest $request, ProgramEducationalObjective $peo)
+    {
+        try {
+            $peo->missions()->detach($request->validated());
+
+            return response()->json([
+                'message' => 'Missions unmapped to PEO successfully'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'failed to unmap missions to PEO',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    // Next step:
+    /* 
+        1. Make Tests on both the request and the controller
+        2.Understand the attach and detach function
+    */
+}
