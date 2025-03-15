4e31efea75afe974624a75045d549c7842fa0f1e
Orekidesu <cmcodog10@gmail.com>
2025-03-16 03:19:33 +0800

    feat: add many-to-many relationship between Mission and ProgramEducationalObjective models

--- app/Models/ProgramEducationalObjective.php
+++ app/Models/ProgramEducationalObjective.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ProgramEducationalObjective extends Model
 {
@@ -20,5 +21,10 @@ class ProgramEducationalObjective extends Model
     public function program(): BelongsTo
     {
         return $this->belongsTo(Program::class, 'program_id');
+    }
+
+    public function missions(): BelongsToMany
+    {
+        return $this->belongsToMany(Mission::class, 'program_educational_objective_mission', 'peo_id', 'mission_id');
     }
 }
