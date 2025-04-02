9ae96c2f359d3b232e7d33d61284813684113126
Orekidesu <cmcodog10@gmail.com>
2025-04-02 22:02:59 +0800

    Add CourseResource for transforming Course model data into JSON format

---
+++ app/Http/Resources/Api/V1/Department/CourseResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Department;
+
+use App\Http\Resources\Api\V1\Admin\DepartmentResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'code' => $this->code,
+            'descriptive_title' => $this->descriptive_title,
+            'department' => new DepartmentResource($this->whenLoaded('department')),
+        ];
+    }
+}
