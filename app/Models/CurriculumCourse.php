e772480c029cc72d04c4a77d1d17b3e857f1192e
Orekidesu <cmcodog10@gmail.com>
2025-04-03 17:32:59 +0800

    Add CurriculumCourse model, factory, and migration for curriculum course management

---
+++ app/Models/CurriculumCourse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CurriculumCourse extends Model
+{
+    use HasFactory;
+}
