f185140ca18c85a38128084e75d5da1d8cf21ce5
Orekidesu <cmcodog10@gmail.com>
2025-04-15 14:01:08 +0800

    Implement program proposal submission logic in ProgramProposalWizardController with validation and transaction handling

--- app/Http/Controllers/Api/V1/Department/ProgramProposalWizardController.php
+++ app/Http/Controllers/Api/V1/Department/ProgramProposalWizardController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers\Api\V1\Department;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Department\ProgramProposalWizardRequest;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Program;
+use App\Models\ProgramProposal;
+use App\Models\Semester;
 
 class ProgramProposalWizardController extends Controller
 {
@@ -16,6 +21,255 @@ class ProgramProposalWizardController extends Controller
     }
 
 
-    public function submit(ProgramProposalWizardRequest $request) {}
+    // Note: this controller is made with proposing a program in mind
+    // not revising a proposed program
+    public function submit(ProgramProposalWizardRequest $request)
+    {
+        DB::beginTransaction();
+
+        $validated = $request->validated();
+
+        try {
+            /*1. Create Program or Get Existing program */
+            $programData  = $validated['program'];
+
+            // Check existing program proposal
+            $existingPending = Program::where('name', $programData['name'])
+                ->where('abbreviation', $programData['abbreviation'])
+                ->where('status', 'pending')
+                ->exists();
+
+            if ($existingPending) {
+                DB::rollBack(); // Close the transaction before returning
+                return response()->json([
+                    'message' => 'A pending version of program already exists',
+                ], 409);
+            }
+
+            // Determine Program Version
+            $latestVersion =  Program::where('name', $programData['name'])
+                ->where('abbreviation', $programData['abbreviation'])
+                ->max('version');
+            $newVersion = $latestVersion ? $latestVersion + 1 : 1;
+
+            // Create Program
+            $program = Program::create([
+                'name' => $programData['name'],
+                'abbreviation' => $programData['abbreviation'],
+                'department_id' => auth()->user()->department_id,
+                'version' => $newVersion,
+                'status' => 'pending',
+            ]);
+
+            // Create Proposal
+            $proposal = ProgramProposal::create([
+                'program_id' => $program->id,
+                'abbreviation' => $program->abbreviation,
+                'version' => $program->version,
+                'status' => 'pending',
+                'comment' =>  null,
+            ]);
+
+            /*2. PEOs */
+            $peos = $validated['peos'];
+            $peoIndexMap = [];
+
+            foreach ($peos as $index => $peoData) {
+                $peo = $program->programEducationalObjective()->create(['statement' => $peoData['statement']]);
+                $peoIndexMap[$index] = $peo->id;
+            }
+
+            /*3. PEO to Mission */
+            $peoMissionMappings = $validated['peo_to_mission_mappings'];
+            foreach ($peoMissionMappings as $map) {
+
+                $peoId = $peoIndexMap[$map['peo_index']];
+
+                DB::table('program_educational_objective_mission')->insert([
+                    'peo_id' => $peoId,
+                    'mission_id' => $map['mission_id'],
+                ]);
+            }
+            /*4. GA to PEO */
+            $gaPeoMappings = $validated['ga_peo_mappings'];
+
+            foreach ($gaPeoMappings as $map) {
+                $peoId = $peoIndexMap[$map['peo_index']];
+
+                DB::table('graduate_attribute_peo')->insert([
+                    'ga_id' => $map['ga_id'],
+                    'peo_id' => $peoId,
+                ]);
+            }
+
+            /*5. Program Outcomes */
+            $pos = $validated['pos'];
+            $poIndexMap = [];
+
+            foreach ($pos as $index => $poData) {
+                $po = $program->programOutcomes()->create([
+                    'name' => $poData['name'],
+                    'statement' => $poData['statement'],
+                ]);
+                $poIndexMap[$index] = $po->id;
+            }
+
+            /*6. PO To PEO */
+            $poPeoMappings = $validated['po_peo_mappings'];
+            foreach ($poPeoMappings as $map) {
+                $poId = $poIndexMap[$map['po_index']];
+                $peoId = $peoIndexMap[$map['peo_index']];
+
+                DB::table('program_outcome_peo')->insert([
+                    'po_id' => $poId,
+                    'peo_id' => $peoId,
+                ]);
+            }
+
+
+            /*7. PO to GA */
+            $poGaMappings = $validated['po_ga_mappings'];
+            foreach ($poGaMappings as $map) {
+                $poId = $poIndexMap[$map['po_index']];
+
+                DB::table('program_outcome_ga')->insert([
+                    'po_id' => $poId,
+                    'ga_id' => $map['ga_id'],
+                ]);
+            }
+            /*8. Curriculum */
+            $curriculumData = $validated['curriculum'];
+            $curriculum = $program->curriculum()->create([
+                'name' => $curriculumData['name'],
+            ]);
+            /*9. Semester */
+            $semesters = $validated['semesters'];
+            $semesterMap = [];
+
+            foreach ($semesters as $semesterData) {
+                // use first or create for unique
+                $semester = Semester::firstOrCreate([
+                    'year' => $semesterData['year'],
+                    'sem' => $semesterData['sem'],
+                ]);
+                // will be use later for curriculum_course
+                $key = $semesterData['year'] . '-' . $semesterData['sem'];
+                $semesterMap[$key] = $semester->id;
+            }
+
+
+            /*10. Course Category */
+            $courseCategories = $validated['course_categories'];
+            $categoryMap = [];
+
+            foreach ($courseCategories as $categoryData) {
+                $category = $curriculum->courseCategories()->create([
+                    'name' => $categoryData['name'],
+                    'code' => $categoryData['code'],
+                ]);
+
+                $categoryMap[$categoryData['code']] = $category->id;
+            }
+
+            /*11. Courses and Curriculum Courses */
+
+            $courses = $validated['courses'];
+            $curriculumCourses = $validated['curriculum_courses'];
+            $courseMap = [];
+            $departmentId = auth()->user()->department_id;
+
+            // First create or update courses
+            foreach ($courses as $courseData) {
+                // check if the course exist
+
+                $existingCourse = DB::table('courses')->where('code', $courseData['code'])->first();
+
+                if ($existingCourse) {
+                    $cousrseID = $existingCourse->id;
+                } else {
+                    // Create new course
+                    DB::table('courses')->insert([
+                        'code' => $courseData['code'],
+                        'descriptive_title' => $courseData['descriptive_title'],
+                        'department_id' => $departmentId,
+
+                    ]);
+                    $courseId = DB::getPdo()->lastInsertId();
+                }
+                $courseMap[$courseData['code']] = $courseId;
+            }
+
+            // Create Curriculum Courses
+            foreach ($curriculumCourses as $ccData) {
+                $semesterKey = $ccData['year'] . '-' . $ccData['name'];
+
+                DB::table('curriculum_courses')->insert([
+                    'curriculum_id' => $curriculum->id,
+                    'course_id' => $courseMap[$ccData['course_code']],
+                    'course_category_id' => $categoryMap[$ccData['category_code']],
+                    'semester_id' => $semesterMap[$semesterKey],
+                    'unit' => $ccData['units'],
+                ]);
+            }
+
+            /*12. Curriculum Course to PO with IRD */
+
+            $coursePOMappings = $validated['course_po_mappings'];
+            foreach ($coursePOMappings as $mapping) {
+                $courseId = $courseMap[$mapping['course_code']];
+                $poCode = $mapping['po_code'];
+
+                // find the PO ID from the name/code
+                $poId = null;
+
+                // find po id
+                foreach ($poIndexMap as $index => $id) {
+                    if ($pos[$index]['name'] === $poCode) {
+                        $poId = $id;
+                        break;
+                    }
+                }
+
+                if (!$poId) {
+                    continue; // skip if poid is not found
+                }
+
+                $curriculumCourseId = DB::table('curriculum_courses')
+                    ->where('curriculum_id', $curriculum->id)
+                    ->where('course_id', $courseId)
+                    ->value('id');
+                if (!$curriculumCourseId) {
+                    continue; // skip if curriculum course is not found
+                }
+
+                // insert each IRD value
+                foreach ($mapping['ird'] as $ird) {
+                    DB::table('curriculum_course_po')->insert([
+                        'curriculum_course_id' => $curriculumCourseId,
+                        'po_id' => $poId,
+                        'ird' => $ird,
+                    ]);
+                }
+            }
+
+
+
+
+
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'proposal submitted successfully',
+            ]);
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return response()->json([
+                'messsage' => 'Failed to submit proposal',
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
     //
 }
