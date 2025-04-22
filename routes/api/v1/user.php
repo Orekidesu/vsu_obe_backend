edfab34f99dae636fa358e40536eedcbb11a10b2
Orekidesu <cmcodog10@gmail.com>
2025-04-22 09:33:39 +0800

    Refactor user response structure to replace 'Username' with 'Email' in user API response

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
   return response([
     'data' => [
-      'Username' => $request->user()->email,
+      'Email' => $request->user()->email, //Username Before
+      'First_Name' => $request->user()->first_name,
+      'Last_Name' => $request->user()->last_name,
       'Role' => $request->user()->role->name,
       'Department' => $request->user()->department ? $request->user()->department : null,
       'Faculty' => !($request->user()->department) ? $request->user()->faculty : null,
