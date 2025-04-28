d8fd56e108c9eac5825724f5876e0532ee33586d
Orekidesu <cmcodog10@gmail.com>
2025-04-28 16:28:54 +0800

    Add CourseOutcomeABCD model with relationship to CourseOutcome

---
+++ app/Models/CourseOutcomeABCD.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CourseOutcomeABCD extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'co_id',
+        'audience',
+        'behaviour',
+        'condition',
+        'degree',
+    ];
+
+    public function courseOutcome(): BelongsTo
+    {
+        return $this->belongsTo(CourseOutcome::class, 'co_id');
+    }
+}
